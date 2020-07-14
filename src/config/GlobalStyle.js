4354457476a22d942bd6a1fe45f4da93129647ef
Ethan <ebonsignori@gmail.com>
2020-07-14 10:42:38 -0400

    add GlobalStyles

---
+++ src/config/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    font-family: 'Roboto', sans-serif;
+    background: #242424;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
