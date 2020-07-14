6ccf0d8651748674944e73c16a4d63b3996971fd
Ethan <ebonsignori@gmail.com>
2020-07-14 14:29:58 -0400

    import options, remove unecessary brackets

--- src/components/DailyChart.js
+++ src/components/DailyChart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-import { dailyOptions } from '../config/dailyChartConfig';
+import { dailyOptions } from '../config/chartOptions';
 import { removeTypeProperty } from '../utils/helpers';
 
 const DailyChart = props => {
@@ -8,16 +8,16 @@ const DailyChart = props => {
     datasets: [
       {
         label: 'Unanswered',
-        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-        borderColor: ['rgba(255, 99, 132, 1)'],
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
         pointBorderColor: 'rgba(255, 99, 132, 1)',
         pointBackgroundColor: 'rgba(179, 52, 79, 1)',
         data: removeTypeProperty(props.unansweredData),
       },
       {
         label: 'Answered',
-        backgroundColor: ['rgba(99, 255, 124, 0.2)'],
-        borderColor: ['rgba(99, 255, 124, 1)'],
+        backgroundColor: 'rgba(99, 255, 124, 0.2)',
+        borderColor: 'rgba(99, 255, 124, 1)',
         pointBorderColor: 'rgba(99, 255, 124, 1)',
         pointBackgroundColor: 'rgba(99, 255, 124, 0.2)',
         data: removeTypeProperty(props.answeredData),
