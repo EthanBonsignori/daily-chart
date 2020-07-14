f557fa9aae12c4a263da13a609701747d45cf016
Ethan <ebonsignori@gmail.com>
2020-07-14 10:41:28 -0400

    add webpack configs for build and dev

---
+++ webpack.build.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const BabiliPlugin = require('babili-webpack-plugin')
+// Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
+const defaultInclude = path.resolve(__dirname, 'src');
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        use: [{ loader: 'babel-loader' }],
+        include: defaultInclude,
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude,
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude,
+      },
+    ],
+  },
+  target: 'electron-renderer',
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    // new BabiliPlugin()
+  ],
+  stats: {
+    colors: true,
+    children: false,
+    chunks: false,
+    modules: false,
+  },
+};
---
+++ webpack.dev.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { spawn } = require('child_process');
+// Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
+const defaultInclude = path.resolve(__dirname, 'src');
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        use: [{ loader: 'babel-loader' }],
+        include: defaultInclude,
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude,
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }],
+        include: defaultInclude,
+      },
+    ],
+  },
+  target: 'electron-renderer',
+  node: {
+    __dirname: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    new webpack.DefinePlugin({
+      __static: `"${path.join(__dirname, '/static').replace(/\\/g, '\\\\')}"`,
+    }),
+  ],
+  devtool: 'cheap-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    stats: {
+      colors: true,
+      chunks: false,
+      children: false,
+    },
+    before() {
+      spawn(
+        'electron',
+        ['.'],
+        { shell: true, env: process.env, stdio: 'inherit' }
+      )
+        .on('close', code => process.exit(0))
+        .on('error', spawnError => console.error(spawnError));
+    },
+  },
+};
