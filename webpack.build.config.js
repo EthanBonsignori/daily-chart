f225f5616c55c9a983a916c6465381491e9b6dcf
Ethan <ebonsignori@gmail.com>
2020-07-15 10:14:14 -0400

    remove comment

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -32,7 +32,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-    // new BabiliPlugin()
   ],
   stats: {
     colors: true,
