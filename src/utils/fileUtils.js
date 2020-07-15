c13240c8c7ecc3d19faa31c2ed8f2bd12d7faf69
Ethan <ebonsignori@gmail.com>
2020-07-15 10:59:35 -0400

    handle data organization in util func instead of App.js

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -6,15 +6,30 @@ import {
   DATA_FILE_DIR,
 } from './constants';
 
-export const readDataFromFile = () => {
+export const getDailyDataFromFile = () => {
+  const dailyDataset1 = [];
+  const dailyDataset2 = [];
   let data = {};
-  if (!fs.existsSync(DAILY_FILE_PATH)) {
+  if (fs.existsSync(DAILY_FILE_PATH)) {
+    const rawData = fs.readFileSync(DAILY_FILE_PATH, 'utf8');
+    data = JSON.parse(rawData);
+  } else {
     console.info(`No file exists at ${DAILY_FILE_PATH}. \nOne will be created when any data is added.`);
-    return data;
   }
-  const rawData = fs.readFileSync(DAILY_FILE_PATH, 'utf8');
-  data = JSON.parse(rawData);
-  return data;
+  for (let i = 0; i < data.length; i += 1) {
+    const { type, x, y } = data[i];
+    if (type === 'unanswered') {
+      dailyDataset1.push({ x, y });
+    }
+    if (type === 'answered') {
+      dailyDataset2.push({ x, y });
+    }
+  }
+  return {
+    dailyDataset1,
+    dailyDataset2,
+    data,
+  };
 };
 
 export const writeDataToFile = data => {
@@ -32,8 +47,8 @@ export const getDataFromFiles = days => {
     if (fs.existsSync(`${DATA_FILE_DIR}/${filenames[i]}`)) {
       const dayData = fs.readFileSync(`${DATA_FILE_DIR}/${filenames[i]}`, 'utf8');
       const parsedDayData = JSON.parse(dayData);
-      unansweredCalls.push(countPropertyInArrOfObj(parsedDayData, 'unanswered'));
-      answeredCalls.push(countPropertyInArrOfObj(parsedDayData, 'answered'));
+      unansweredCalls.push(countPropertyInArrOfObj(parsedDayData, 'unanswered') - 1);
+      answeredCalls.push(countPropertyInArrOfObj(parsedDayData, 'answered') - 1);
     } else {
       unansweredCalls.push(0);
       answeredCalls.push(0);
