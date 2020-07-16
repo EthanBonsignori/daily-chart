597bfb23d08351268d4a7dcf73255954b8d6768f
Ethan <ebonsignori@gmail.com>
2020-07-16 13:23:29 -0400

    remove comments, check for Dev differently, working React dev tools

--- src/main.js
+++ src/main.js
@@ -1,41 +1,32 @@
-// Import parts of electron to use
-const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
-// Add React extension for development
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const os = require('os');
+const { app, session, BrowserWindow } = require('electron');
+
+const DEV_DIR = '/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.8.1_0';
 let mainWindow;
-// Keep a reference for dev mode
-let dev = false;
-// Determine the mode (dev or production)
-if (process.defaultApp
-    || /[\\/]electron-prebuilt[\\/]/.test(process.execPath)
-    || /[\\/]electron[\\/]/.test(process.execPath)
-) {
-  dev = true;
-}
+
+const dev = (process.env.ELECTRON_ENV === 'development');
+console.log(process.env.ELECTRON_ENV);
+
 // Temporary fix for broken high-dpi scale factor on Windows (125% scaling)
 // info: https://github.com/electron/electron/issues/9691
 if (process.platform === 'win32') {
   app.commandLine.appendSwitch('high-dpi-support', 'true');
   app.commandLine.appendSwitch('force-device-scale-factor', '1');
 }
+
 function createWindow() {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1024, // width of the window
-    height: 768, // height of the window
-    show: false, // don't show until window is ready
+    width: 1400,
+    height: 800,
+    show: false,
     webPreferences: {
       nodeIntegration: true,
     },
   });
-  // and load the index.html of the app.
+
   let indexPath;
-  // Determine the correct index.html file
-  // (created by webpack) to load in dev and production
   if (dev && process.argv.indexOf('--noDevServer') === -1) {
     indexPath = url.format({
       protocol: 'http:',
@@ -50,15 +41,12 @@ function createWindow() {
       slashes: true,
     });
   }
-  // Load the index.html
   mainWindow.loadURL(indexPath);
-  // Don't show the app window until it is ready and loaded
+
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
     // Open the DevTools automatically if developing
     if (dev) {
-      installExtension(REACT_DEVELOPER_TOOLS)
-        .catch(err => console.error('Error loading React DevTools: ', err));
       mainWindow.webContents.openDevTools();
     }
     if (!dev) {
@@ -68,29 +56,28 @@ function createWindow() {
       global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\');
     }
   });
-  // Emitted when the window is closed.
+
   mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 }
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-// Quit when all windows are closed.
+
+app.on('ready', async () => {
+  if (dev) {
+    await session.defaultSession.loadExtension(
+      path.join(os.homedir(), DEV_DIR)
+    );
+  }
+  createWindow();
+});
+
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
