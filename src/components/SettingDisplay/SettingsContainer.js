a546c444768d5d2724a5b64e40ea461123bc6dce
Ethan <ebonsignori@gmail.com>
2020-07-17 14:19:31 -0400

    change flex dir, wrap

--- src/components/SettingDisplay/SettingsContainer.js
+++ src/components/SettingDisplay/SettingsContainer.js
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 export default styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   box-sizing: border-box;
-
+  flex-wrap: wrap;
+  
   color: #fff;
   background: #333333;
   width: 100%;
