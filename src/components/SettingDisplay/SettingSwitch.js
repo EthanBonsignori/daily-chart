9d85746c49a89211c227eaef0753af668bda5dcd
Ethan <ebonsignori@gmail.com>
2020-07-17 14:20:58 -0400

    deconstruct props in args, css changes

--- src/components/SettingDisplay/SettingSwitch.js
+++ src/components/SettingDisplay/SettingSwitch.js
@@ -5,31 +5,31 @@ import Switch from 'react-switch';
 const offColor = '#858585';
 const onColor = '#4abd5c';
 
-const SettingSwitch = props => (
+const SettingSwitch = ({
+  label,
+  id,
+  onChange,
+  checked,
+}) => (
   <SwitchLabel>
+    {label}
     <Switch
-      id={props.id}
-      onChange={props.onChange}
-      checked={props.checked}
+      id={id}
+      onChange={onChange}
+      checked={checked}
       offColor={offColor}
       onColor={onColor}
       width={40}
       height={16}
     />
-    <SwitchText>{props.label}</SwitchText>
   </SwitchLabel>
 );
 
 const SwitchLabel = styled.label`
-  /* display: flex; */
-  /* align-items: center; */
-  /* margin-left: 10px; */
-  /* margin-bottom: 3px; */
-`;
-
-const SwitchText = styled.span`
-  color: #fff;
-  margin: 0 10px;
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+  margin-bottom: 3px;
 `;
 
 export default SettingSwitch;
