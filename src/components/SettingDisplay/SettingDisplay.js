1155fbff68f8d3720ca848b7cd0ed4f150e65a09
Ethan <ebonsignori@gmail.com>
2020-07-17 14:18:32 -0400

    move Column control to App.js

--- src/components/SettingDisplay/SettingDisplay.js
+++ src/components/SettingDisplay/SettingDisplay.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import SettingsContainer from './SettingsContainer';
 import SettingsTitle from './SettingsTitle';
-import SettingsColumn from './SettingsColumn';
 
 const SettingDisplay = ({ children }) => (
   <SettingsContainer>
     <SettingsTitle>Settings</SettingsTitle>
-    <SettingsColumn leftLabel='Feature' centerLabel='Display Text' rightLabel='Hide/Show'>
-      {children}
-    </SettingsColumn>
+    {children}
   </SettingsContainer>
 );
 
