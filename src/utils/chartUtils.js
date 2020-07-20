4892cb9887273c00bc1f6805307b220e45c7401a
Ethan <ebonsignori@gmail.com>
2020-07-20 10:45:58 -0400

    fix show/hide weekend dates

--- src/utils/chartUtils.js
+++ src/utils/chartUtils.js
@@ -2,15 +2,16 @@ import moment from 'moment';
 import { getUserSettingsFromFile } from '../config/userSettings';
 
 export const generateFilenames = days => {
-  const userSettings = getUserSettingsFromFile();
+  const { showWeekends } = getUserSettingsFromFile();
   const filenames = [];
   let i = 0;
   while (filenames.length < days) {
     const filenameDate = moment().subtract(i, 'days').format('MM-DD-YYYY');
     const dayOfTheWeek = moment(filenameDate, 'MM-DD-YYYY').isoWeekday();
-    if (userSettings.showWeekends && dayOfTheWeek !== 6 && dayOfTheWeek !== 7) {
+    if (showWeekends) {
       filenames.push(`${filenameDate}.json`);
-    } else if (!userSettings.showWeekends) {
+      // dayOfTheWeek 6|7 === saturday|sunday
+    } else if (!showWeekends && dayOfTheWeek < 6) {
       filenames.push(`${filenameDate}.json`);
     }
     i += 1;
